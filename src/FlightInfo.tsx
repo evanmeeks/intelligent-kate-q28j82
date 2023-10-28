f4e7cd2d00c7ffcb45ed4403987bf9c6a894bad2
Evan Meeks <evan.meeks@gmail.com>
2023-10-28 10:16:23 -0500

    Update FlightInfo.tsx

--- src/FlightInfo.tsx
+++ src/FlightInfo.tsx
@@ -1,13 +1,128 @@
 import React, { useState, useEffect } from "react";
+export interface APIResponse {
+  departures: DepartureElement[];
+  arrivals: ArrivalElement[];
+}
+
+export interface ArrivalElement {
+  departure: ArrivalDeparture;
+  arrival: DepartureClass;
+  number: string;
+  callSign?: string;
+  status: Status;
+  codeshareStatus: CodeshareStatus;
+  isCargo: boolean;
+  aircraft: Aircraft;
+  airline: Air;
+  location?: Location;
+}
+
+export interface Aircraft {
+  reg?: string;
+  modeS?: string;
+  model: string;
+}
+
+export interface Air {
+  name: string;
+  iata?: string;
+  icao?: string;
+}
+
+export interface DepartureClass {
+  scheduledTime: Time;
+  revisedTime?: Time;
+  quality: Quality[];
+}
+
+export enum Quality {
+  Basic = "Basic",
+  Live = "Live",
+}
+
+export interface Time {
+  utc: string;
+  local: string;
+}
+
+export enum CodeshareStatus {
+  IsOperator = "IsOperator",
+  Unknown = "Unknown",
+}
+
+export interface ArrivalDeparture {
+  airport: Air;
+  scheduledTime?: Time;
+  revisedTime?: Time;
+  runwayTime?: Time;
+  quality: Quality[];
+  terminal?: string;
+}
+
+export interface Location {
+  pressureAltitude: Altitude;
+  altitude: Altitude;
+  pressure: { [key: string]: number };
+  groundSpeed: GroundSpeed;
+  trueTrack: TrueTrack;
+  reportedAtUtc: string;
+  lat: number;
+  lon: number;
+}
+
+export interface Altitude {
+  meter: number;
+  km: number;
+  mile: number;
+  nm: number;
+  feet: number;
+}
+
+export interface GroundSpeed {
+  kt: number;
+  kmPerHour: number;
+  miPerHour: number;
+  meterPerSecond: number;
+}
+
+export interface TrueTrack {
+  deg: number;
+  rad: number;
+}
+
+export enum Status {
+  Expected = "Expected",
+  Unknown = "Unknown",
+}
+
+export interface DepartureElement {
+  departure: DepartureClass;
+  arrival: PurpleArrival;
+  number: string;
+  callSign?: string;
+  status: Status;
+  codeshareStatus: CodeshareStatus;
+  isCargo: boolean;
+  aircraft: Aircraft;
+  airline: Air;
+}
+
+export interface PurpleArrival {
+  airport: Air;
+  scheduledTime: Time;
+  revisedTime?: Time;
+  terminal?: string;
+  quality: Quality[];
+}
 
 const FlightInfo = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<APIResponse | null>(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     // Helper function to format the date as required by the API
-    const formatDateToString = (date) => {
+    const formatDateToString = (date: Date) => {
       const year = date.getFullYear();
       let month = date.getMonth() + 1;
       let day = date.getDate();
@@ -15,10 +130,10 @@ const FlightInfo = () => {
       let minute = date.getMinutes();
 
       // Padding single digits with zero
-      month = month < 10 ? "0" + month : month;
-      day = day < 10 ? "0" + day : day;
-      hour = hour < 10 ? "0" + hour : hour;
-      minute = minute < 10 ? "0" + minute : minute;
+      month = Number(month < 10 ? "0" + month : month);
+      day = Number(day < 10 ? "0" + day : day);
+      hour = Number(hour < 10 ? "0" + hour : hour);
+      minute = Number(minute < 10 ? "0" + minute : minute);
 
       return `${year}-${month}-${day}T${hour}:${minute}`;
     };
@@ -52,8 +167,16 @@ const FlightInfo = () => {
         } else {
           setError(`Error: ${response.statusText}`);
         }
-      } catch (err) {
-        setError(`Error: ${err.message}`);
+      } catch (err: unknown) {
+        // Change here to 'unknown'
+        // We need to check if 'err' is an instance of Error to satisfy TypeScript's type checker
+        if (err instanceof Error) {
+          setError(`Error: ${err.message}`);
+        } else {
+          // If it's not an Error instance, it could be anything, so we might want to stringify it.
+          // You can handle different cases based on your needs.
+          setError(`An error occurred: ${String(err)}`);
+        }
       } finally {
         setLoading(false);
       }
@@ -62,26 +185,16 @@ const FlightInfo = () => {
     fetchData();
   }, []);
 
-  const format12HourTime = (dateString) => {
-    // Create a date object from the date string
+  const format12HourTime = (dateString: Date) => {
     const date = new Date(dateString);
-
-    // Extract hours and minutes
     let hours = date.getHours();
-    let minutes = date.getMinutes();
-
-    // Calculate AM or PM
+    const minutes = date.getMinutes(); // Keep minutes as a number here
     const amOrPm = hours >= 12 ? "PM" : "AM";
-
-    // Convert hours to 12-hour format
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
 
-    // Pad minutes with a zero if needed
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-
-    // Return the formatted string
-    return `${hours}:${minutes} ${amOrPm}`;
+    // Minutes converted to string only in the return statement
+    return `${hours}:${minutes < 10 ? "0" + minutes : minutes} ${amOrPm}`;
   };
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
@@ -89,7 +202,7 @@ const FlightInfo = () => {
   return (
     <div>
       <h1>Arrivals</h1>
-      {data.arrivals.map((flight: any, index: number) => (
+      {data?.arrivals.map((flight: any, index: number) => (
         <div key={index}>
           <h2>{flight.airline ? flight.airline.name : "Private Flight"}</h2>
           <p>Flight Number: {flight.number}</p>
